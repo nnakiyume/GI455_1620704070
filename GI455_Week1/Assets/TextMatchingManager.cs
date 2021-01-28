c2b34cea3979cc10f03b2b6c997ccf59c7c8da10
NNAkiyume <nn.akiyume@gmail.com>
2021-01-28 13:18:27 +0700

    Send Assignment Week1

---
+++ GI455_Week1/Assets/TextMatchingManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+public class TextMatchingManager : MonoBehaviour
+{
+    // Properties
+    [SerializeField] private Text TextDataUi;
+    [SerializeField] private Text ResultTextUi;
+    [SerializeField] private InputField TextInputField;
+
+    private string textInput;
+    [SerializeField] private string[] dataText;
+
+    // Behavior
+    void Start()
+    {
+        dataText = new string[] {
+            "Unity",
+            "Unreal",
+            "ResidentEvil",
+            "Minecraft",
+            "Google",
+            "MongoDB",
+            "Nutthanon"
+        };
+        
+        UpdateDataTextUI();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            MatchingText(TextInputField.text);
+        }
+    }
+
+    public void UpdateDataTextUI()
+    {
+        foreach (var text in dataText)
+        {
+            TextDataUi.text += text + "\n";
+        }
+    }
+
+    public void OnFindClick()
+    {
+        MatchingText(TextInputField.text);
+    }
+    
+    public void MatchingText(string text)
+    {
+        if (dataText.Contains(text))
+        {
+            ResultTextUi.text = $"\" <color=#3CB371>{text}</color> \" is found.\n";
+        }
+        else
+        {
+            ResultTextUi.text = $"\" <color=#DC143C>{text}</color> \" is not found.\n";
+        }
+    }
+}
